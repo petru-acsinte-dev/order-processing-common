0ee811222b09c0eed208303e9fd771656fde0ada
petruacsinte <petruacsinte@hotmail.com>
2026-03-24 09:46:24 -0400

    Adding global mapper configuration to common library.

---
+++ src/main/java/com/orderprocessing/common/mappers/GlobalMapperConfig.java
@@ -0,0 +1,12 @@
+package com.orderprocessing.common.mappers;
+
+import org.mapstruct.MapperConfig;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+
+@MapperConfig (componentModel = "spring",
+	nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+	unmappedTargetPolicy = ReportingPolicy.ERROR)
+public interface GlobalMapperConfig {
+
+}
