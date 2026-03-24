1ae871c3b0dc8c52eccf3521e9c787ca16f3b050
petruacsinte <petruacsinte@hotmail.com>
2026-03-24 15:15:51 -0400

    0.0.9: making common IT class Spring agnostic

--- src/test/java/com/orderprocessing/common/tests/AbstractIntegrationTestBase.java
+++ src/test/java/com/orderprocessing/common/tests/AbstractIntegrationTestBase.java
@@ -5,19 +5,15 @@ import static com.orderprocessing.common.tests.SharedPostgresContainer.POSTGRES;
 import org.junit.jupiter.api.Tag;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Tag("integration")
-@SpringBootTest
-@AutoConfigureMockMvc
-@ActiveProfiles("test")
+@Testcontainers
 public abstract class AbstractIntegrationTestBase {
 
 	protected static final String UUID_REGEX = "[a-fA-F0-9]{8}-([a-fA-F0-9]{4}-){3}[a-fA-F0-9]{12}"; //$NON-NLS-1$
